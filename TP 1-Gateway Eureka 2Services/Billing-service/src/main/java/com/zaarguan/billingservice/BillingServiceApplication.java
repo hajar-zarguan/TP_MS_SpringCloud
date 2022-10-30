89fa9c95bd52d83ae78ca132eae4b0760f88c11b
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-10-30 23:59:21 +0100

    DockerFiles

--- TP 1-Gateway Eureka 2Services/Billing-service/src/main/java/com/zaarguan/billingservice/BillingServiceApplication.java
+++ TP 1-Gateway Eureka 2Services/Billing-service/src/main/java/com/zaarguan/billingservice/BillingServiceApplication.java
@@ -1,5 +1,4 @@
 package com.zaarguan.billingservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
