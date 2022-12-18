9ca299193bf23e48c74293942816df6738628433
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-18 21:55:44 +0100

    Axon

---
+++ TP 4- AxonIQ/CompteCQRS/src/main/java/com/hajarzarguan/comptecqrs/CompteCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.hajarzarguan.comptecqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteCqrsApplication.class, args);
+    }
+
+}
